f6c8293570a04c299edb04558206449acfb8435c
George Ogden <george@goprogram.ai>
2023-04-30 21:08:42 +0100

    update tests to simulator

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,4 +1,3 @@
-from dm_env.specs import BoundedArray
 import numpy as np
 import torch
 
@@ -28,7 +27,7 @@ def test_simulation_observations():
         assert len(observations) == 4
         for observation in observations:
             spec.validate_observation(observation.detach().numpy())
-        observations, rewards = simulator.step(
+        observations, rewards, values = simulator.step(
             torch.tensor(
                 np.random.uniform(
                     low=spec.action_spec.minimum,
@@ -42,7 +41,7 @@ def test_simulation_observations():
 def test_simulation_rewards():
     observations = simulator.reset(4)
     for _ in range(100):
-        observations, rewards = simulator.step(
+        observations, rewards, values = simulator.step(
             torch.tensor(
                 np.random.uniform(
                     low=spec.action_spec.minimum,
@@ -55,3 +54,20 @@ def test_simulation_rewards():
         assert rewards.shape == (4,)
         for reward in rewards:
             assert 0 <= reward and reward <= 1
+
+def test_simulation_values():
+    observations = simulator.reset(4)
+    for _ in range(100):
+        observations, rewards, values = simulator.step(
+            torch.tensor(
+                np.random.uniform(
+                    low=spec.action_spec.minimum,
+                    high=spec.action_spec.maximum,
+                    size=(4,) + spec.action_spec.shape,
+                ),
+                dtype=torch.float32
+            )
+        )
+        assert values.shape == (4,)
+        for value in values:
+            assert 0 <= value and value <= 1 / (1 - simulator.discount)
